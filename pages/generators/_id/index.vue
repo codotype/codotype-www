ef3eee05ac4b2c8692a33518a60cd7bac060cb94
Alexander Schwartzberg <aeksco@gmail.com>
2019-05-27 23:24:05 -0400

    Generator show page uses generator.description for meta.description

--- pages/generators/_id/index.vue
+++ pages/generators/_id/index.vue
@@ -19,7 +19,7 @@ export default {
         {
           hid: 'description',
           name: 'description',
-          content: 'My custom description'
+          content: this.model.description
         }
       ]
     }
