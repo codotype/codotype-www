63f568a642ce60a9c8da738bcb67b32f3a5e660d
Alexander Schwartzberg <aeksco@gmail.com>
2019-06-08 23:19:58 -0400

    Added new components

---
+++ components/Footer.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="footer" class="section bg-light mt-4">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <span class="d-flex flex-row align-items-center">
+            <img
+              src="https://res.cloudinary.com/codotype/image/upload/v1560045005/tech-logos/codotype.png"
+              style="width: 2rem; height: 2rem;"
+              class="logo-dark"
+              alt="Codotype Logo"
+            />
+            <p class="ml-3 mb-0">
+              <span
+                >Codotype is a code generation tool for the modern web.</span
+              >
+              <br />
+            </p>
+          </span>
+          <div class="mt-4">
+            <gh-btns-star slug="codotype/codotype" show-count />
+          </div>
+        </div>
+        <!-- // end .col-sm-3 -->
+        <div class="col-sm-2">
+          <ul class="list-unstyled footer-links ml-1">
+            <li>
+              <nuxt-link to="/generators">
+                Generators
+              </nuxt-link>
+            </li>
+            <li>
+              <a target="_blank" href="https://codotype.org">
+                Documentation
+              </a>
+            </li>
+          </ul>
+        </div>
+        <!-- // end .col-sm-3 -->
+        <div class="col-sm-2">
+          <ul class="list-unstyled footer-links ml-1">
+            <li>
+              <nuxt-link to="/about">
+                About
+              </nuxt-link>
+            </li>
+          </ul>
+        </div>
+        <!-- // end .col-sm-3 -->
+        <div class="col-sm-2">
+          <ul class="list-unstyled footer-links ml-1">
+            <li>
+              <a target="_blank" href="https://github.com/codotype/codotype">
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href="https://twitter.com/codotype">
+                Twitter
+              </a>
+            </li>
+          </ul>
+        </div>
+        <!-- // end .col-sm-3 -->
+        <div class="col-sm-2">
+          <a href="#home" class="btn btn-sm btn-outline-primary ml-1">
+            Go to Top
+          </a>
+        </div>
+        <!-- // end .col-sm-3 -->
+      </div>
+      <!-- // end .row -->
+      <div class="text-center mt-4">
+        <small class="text-muted">
+          Copyright © {{ new Date().getFullYear() }}
+          Codotype Inc. All rights reserved.
+        </small>
+      </div>
+    </div>
+    <!-- // end .container -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/style.scss';
+
+.footer-links {
+  li {
+    padding: 0.2em 0;
+  }
+  a {
+    color: $gray-light;
+    &:hover {
+      text-decoration: none;
+      color: $brand-primary;
+    }
+  }
+}
+</style>
---
+++ components/RegisterForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="signup" class="section">
+    <div class="container">
+      <div class="section-title text-center">
+        <h3>Start your free trial</h3>
+        <p>Signup now. Its free and takes less than 3 minutes</p>
+      </div>
+      <div class="row justify-content-md-center">
+        <div class="col col-md-5">
+          <form>
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Full Name" />
+            </div>
+            <div class="form-group">
+              <input
+                type="email"
+                class="form-control"
+                placeholder="Email Address"
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="password"
+                class="form-control"
+                placeholder="Password"
+              />
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn btn-xl btn-block btn-primary">
+                GET STARTED FOR FREE
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegisterForm'
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/style.scss';
+.form-control {
+  height: 50px;
+  border-radius: 30px;
+  padding: 5px 20px;
+  height: 55px;
+  border-color: #ebebed;
+  box-shadow: 0 0 5px rgba(38, 38, 76, 0.05);
+}
+
+.btn-block {
+  padding: 15px 0;
+}
+</style>
