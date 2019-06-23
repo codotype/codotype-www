5864a82252e2a1bcdc43d640b0613ed2c1025333
Alexander Schwartzberg <aeksco@gmail.com>
2019-06-22 21:27:35 -0400

    Misc cleanup

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,6 @@
   <div class="overflow-hidden">
     <header id="home">
       <Navbar />
-      <!-- <template v-if="$nuxt.$route.path === '/'"> -->
     </header>
     <nuxt />
     <AppFooter />
@@ -46,109 +45,92 @@ export default {
 }
 </script>
 
-<style lang="scss">
-@import '@/assets/scss/style.scss';
+<style lang="sass">
+@import '@/assets/sass/main.sass';
 
-body {
-  background: white !important;
-  overflow-x: hidden;
-}
+body
+  overflow-x: hidden
 
-[class^='display'] {
-  font-family: $heading-font;
-  font-weight: 700;
-}
+[class^='display']
+  font-family: $heading-font
+  font-weight: 700
 
-@include media-breakpoint-down(md) {
-  .display-3 {
-    font-size: 4rem;
-  }
-  .display-5 {
-    font-size: 1.4rem;
-  }
-}
+@include media-breakpoint-down(md)
+  .display-3
+    font-size: 4rem
 
-@include media-breakpoint-down(sm) {
-  .display-3 {
-    font-size: 2.2rem;
-  }
-  .display-4 {
-    font-size: 2.5rem;
-  }
-}
+  .display-5
+    font-size: 1.4rem
 
-header {
-  position: relative;
-}
+@include media-breakpoint-down(sm)
+  .display-3
+    font-size: 2.2rem
 
-.bg-shape {
-  position: absolute;
-  background-color: $brand-primary;
-  background-image: linear-gradient(100deg, $brand-primary, $brand-secondary);
-  top: -350px;
-  right: -110px;
-  border-radius: 8%;
-  width: 50%;
-  height: 800px;
-  transform: skew(3deg, 30deg);
-  opacity: 1;
-  z-index: -1;
-  @include media-breakpoint-down(md) {
-    top: -150px;
-    right: 0;
-    width: 20%;
-    height: 500px;
-  }
-  @include media-breakpoint-down(sm) {
-    display: none;
-  }
-}
+  .display-4
+    font-size: 2.5rem
 
-.bg-circle {
-  position: absolute;
-  background-color: $brand-primary;
-  background-image: linear-gradient(100deg, $brand-primary, $brand-secondary);
-  top: -400px;
-  left: -350px;
-  border-radius: 100%;
-  height: 800px;
-  width: 800px;
-  opacity: 0.2;
-  z-index: -1;
-  @include media-breakpoint-down(md) {
-    height: 400px;
-    width: 400px;
-    top: -200px;
-    left: -200px;
-  }
-  @include media-breakpoint-down(sm) {
-    display: none;
-  }
-}
+header
+  position: relative
+  z-index: 2
 
-.bg-circle-two {
-  position: absolute;
-  background-color: $brand-primary;
-  background-image: linear-gradient(100deg, $brand-primary, $brand-secondary);
-  top: 150px;
-  left: 350px;
-  border-radius: 100%;
-  height: 100px;
-  width: 100px;
-  opacity: 0.8;
-  z-index: -1;
-  @include media-breakpoint-down(md) {
-    top: 150px;
-    left: 70px;
-    height: 50px;
-    width: 50px;
-  }
-  @include media-breakpoint-down(sm) {
-    display: none;
-  }
-}
+.bg-shape
+  position: absolute
+  background-color: $blue
+  background-image: linear-gradient(100deg, $blue, $teal)
+  top: -350px
+  right: -110px
+  border-radius: 8%
+  width: 50%
+  height: 800px
+  transform: skew(3deg, 30deg)
+  opacity: 1
+  // z-index: -1
+  @include media-breakpoint-down(md)
+    top: -150px
+    right: 0
+    width: 20%
+    height: 500px
+  @include media-breakpoint-down(sm)
+    display: none
 
-.section {
-  position: relative;
-}
+.bg-circle
+  position: absolute
+  background-color: $blue
+  background-image: linear-gradient(100deg, $blue, $teal)
+  top: -400px
+  left: -350px
+  border-radius: 100%
+  height: 800px
+  width: 800px
+  opacity: 0.2
+  // z-index: -1
+  @include media-breakpoint-down(md)
+    height: 400px
+    width: 400px
+    top: -200px
+    left: -200px
+  @include media-breakpoint-down(sm)
+    display: none
+
+.bg-circle-two
+  position: absolute
+  background-color: $blue
+  background-image: linear-gradient(100deg, $blue, $teal)
+  top: 150px
+  left: 350px
+  border-radius: 100%
+  height: 100px
+  width: 100px
+  opacity: 0.8
+  // z-index: -1
+  @include media-breakpoint-down(md)
+    top: 150px
+    left: 70px
+    height: 50px
+    width: 50px
+  @include media-breakpoint-down(sm)
+    display: none
+
+.section
+  position: relative
 </style>
